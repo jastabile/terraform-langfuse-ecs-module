b9e060e9dbfd5d53ac7d9d2c6bbd902ce7816898
Javier Stabile <javier.stabile@qubika.com>
2025-06-30 10:28:34 -0300

    Increase memory allocation for Clickhouse ECS task definition from 2048 to 4096 MB

--- modules/langfuse/ecs_task_definitions.tf
+++ modules/langfuse/ecs_task_definitions.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_task_definition" "clickhouse" {
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 1024
-  memory                   = 2048
+  memory                   = 4096
   execution_role_arn       = aws_iam_role.TaskExecutionRole.arn
   task_role_arn            = aws_iam_role.ecs_task_role_clickhouse.arn
 
