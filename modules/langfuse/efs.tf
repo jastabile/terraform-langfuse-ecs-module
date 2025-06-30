24d08ca255f0ba09be79afd07d21040568df3a69
Javier Stabile <javier.stabile@qubika.com>
2025-06-30 10:25:44 -0300

    Move Clickhouse EFS Access Point configuration from clickhouse.tf to efs.tf

--- modules/langfuse/efs.tf
+++ modules/langfuse/efs.tf
@@ -91,6 +91,30 @@ resource "aws_iam_role_policy_attachment" "efs" {
   role       = aws_iam_role.efs.name
 }
 
+# EFS Access Points for Clickhouse instance
+resource "aws_efs_access_point" "clickhouse" {
+  file_system_id = aws_efs_file_system.langfuse.id
+
+  root_directory {
+    path = "/clickhouse"
+    creation_info {
+      owner_gid   = 101
+      owner_uid   = 101
+      permissions = "0755"
+    }
+  }
+
+  posix_user {
+    gid = 101
+    uid = 101
+  }
+
+  tags = {
+    Name = "${var.name} Clickhouse"
+  }
+}
+
+
 resource "aws_efs_access_point" "clickhouse_logs" {
   file_system_id = aws_efs_file_system.langfuse.id
 
